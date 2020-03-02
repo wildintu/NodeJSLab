b56c3522a44b95a6529ed5435fe779b3ed767b92
wildintu <hello@wildintu.com>
2020-03-02 12:51:44 -0600

    starting advanced section

---
+++ popular.downloader.js
@@ -0,0 +1,23 @@
+// let path = require('path');
+// let fs = require('fs');
+// let rp = require('request-promise');
+
+// let dataPath = path.join(__dirname, '/popular-articles.json');
+
+// rp('https://reddit.com/r/popular.json')
+// .then(function(body) {
+//     let arr = [];
+//     JSON.parse(body).data.children.forEach(item => {
+//         arr.push({
+//             title: item.data.title, 
+//             url: item.data.url,
+//             author: item.data.author})
+//     })
+//     // console.log(arr)
+//     fs.writeFile(dataPath, JSON.stringify(arr), (err) => {
+//         if(err) return console.log(err);
+//     })
+// })
+// .catch(function(err) {
+//     console.log(err);
+// })
---
+++ server/server.js
@@ -0,0 +1,33 @@
+let path = require('path');
+let fs = require('fs');
+
+let dataPath = path.join(__dirname, '../chirps.json');
+
+let chirps = [
+    {name: 'Karen',
+     gender: 'female'
+    },
+    {name: 'Linda',
+     gender: 'female'
+    },
+    {name: 'Joe',
+     gender: 'male'
+    },
+    {name: 'Jack',
+     saying: 'male'
+    },
+    {name: 'Billy',
+     saying: 'female'
+    }
+]
+
+fs.writeFile(dataPath, JSON.stringify(chirps), (err) => {
+    if(err) return console.log(err);
+})
+
+fs.readFile(dataPath,(err,data) => {
+    if(err) console.log(err);
+    JSON.parse(data).forEach(chirp => {
+        console.log(chirp);
+    })
+})
