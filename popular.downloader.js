c318ba573079e34bab2a2fa5c6fa85e170aac36e
wildintu <hello@wildintu.com>
2020-03-02 13:32:15 -0600

    done

--- popular.downloader.js
+++ popular.downloader.js
@@ -1,23 +1,31 @@
-// let path = require('path');
-// let fs = require('fs');
-// let rp = require('request-promise');
+let path = require('path');
+let fs = require('fs');
+let rp = require('request-promise');
 
-// let dataPath = path.join(__dirname, '/popular-articles.json');
+let dataPath = path.join(__dirname, '/downloads');
 
-// rp('https://reddit.com/r/popular.json')
-// .then(function(body) {
-//     let arr = [];
-//     JSON.parse(body).data.children.forEach(item => {
-//         arr.push({
-//             title: item.data.title, 
-//             url: item.data.url,
-//             author: item.data.author})
-//     })
-//     // console.log(arr)
-//     fs.writeFile(dataPath, JSON.stringify(arr), (err) => {
-//         if(err) return console.log(err);
-//     })
-// })
-// .catch(function(err) {
-//     console.log(err);
-// })
+rp('https://reddit.com/r/popular.json')
+.then(function(body) {
+    let article = [];
+    JSON.parse(body).data.children.forEach(item => {
+        article.push({
+            url: item.data.url,
+            author: item.data.author})
+    })
+    // console.log(article)
+    article.forEach(item => {
+        if(path.extname(item.url) === '.jpg' || path.extname(item.url) === '.gif' || path.extname(item.url) === '.png') {
+            let filename = item.author + path.extname(item.url);
+            rp(item.url, {encoding: 'base64'})
+            .then((download) => {
+                fs.writeFile(`${dataPath}/${filename}`,download,{encoding: 'base64'}, (err) => {
+                    if(err) console.log(err)
+                })
+            })
+        }
+        //console.log(path.extname(item.url))
+    })
+})
+.catch(function(err) {
+    console.log(err);
+})
